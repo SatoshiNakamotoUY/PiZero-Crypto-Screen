6d8416d67797cc5bde7fc8d2b4c66d35cf765c15
Andy Lim <shadowandy.sg@gmail.com>
2021-12-08 19:50:24 +0800

    Reverting

--- data/plot.py
+++ data/plot.py
@@ -109,9 +109,6 @@ class Plot:
     def human_format(number, length, fractional_minimal=0):
         magnitude = 0
         num = number
-        # truncate to 2 decimal places
-        number = '%.2f'%({number})
-        return number
         while abs(num) >= 10:
             magnitude += 1
             num /= 10.0
@@ -119,5 +116,4 @@ class Plot:
         if length >= magnitude + fractional_minimal + 2:
             fractional_length = length - magnitude - 2
             format_string = f'%.{fractional_length}f'
-        #return format_string % number
-        return number
+        return format_string % number
