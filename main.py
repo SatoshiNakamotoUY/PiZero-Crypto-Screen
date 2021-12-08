a58a74a99e73ae9f18b155cbf8c7181fa6cddd15
Andy Lim <shadowandy.sg@gmail.com>
2021-12-08 16:09:19 +0800

    Increase coins list

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from datetime import datetime, timezone, timedelta
 from urllib.error import HTTPError, URLError
 from urllib.request import Request, urlopen
 
+import itertools
+
 from config.builder import Builder
 from config.config import config
 from logs import logger
@@ -19,12 +21,12 @@ def get_dummy_data():
 
 
 
-def fetch_prices():
+def fetch_prices(token):
     logger.info('Fetching prices')
     timeslot_end = datetime.now(timezone.utc)
     end_date = timeslot_end.strftime(DATETIME_FORMAT)
     start_data = (timeslot_end - timedelta(days=DATA_SLICE_DAYS)).strftime(DATETIME_FORMAT)
-    url = f'https://production.api.coindesk.com/v2/price/values/{config.currency}?ohlc=true&start_date={start_data}&end_date={end_date}'
+    url = f'https://production.api.coindesk.com/v2/price/values/{token}?ohlc=true&start_date={start_data}&end_date={end_date}'
     req = Request(url)
     data = urlopen(req).read()
     external_data = json.loads(data)
@@ -38,11 +40,12 @@ def main():
     data_sink = Observable()
     builder = Builder(config)
     builder.bind(data_sink)
+    coins = config.currency.split(',')
 
     try:
-        while True:
+        for coin in itertools.cycle(coins):
             try:
-                prices = [entry[1:] for entry in get_dummy_data()] if config.dummy_data else fetch_prices()
+                prices = [entry[1:] for entry in get_dummy_data()] if config.dummy_data else fetch_prices(coin)
                 data_sink.update_observers(prices)
                 time.sleep(config.refresh_interval)
             except (HTTPError, URLError) as e:
